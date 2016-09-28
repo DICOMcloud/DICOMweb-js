bdf6fdd383d6a073e77dce99f8853a6cc80e8968
zaid al-safadi <zsafadi11@gmail.com>
2016-09-28 00:16:03 -0400

    redo the wado-rs proxy interface

--- DICOMwebJS.Proxies/Proxies/WadoRsProxy.ts
+++ DICOMwebJS.Proxies/Proxies/WadoRsProxy.ts
@@ -13,54 +13,28 @@ class WadoRsProxy{
       this._baseUrl = baseUrl;
    }
 
-   public getStudyMetadata
-   (
-      studyInstanceUid: string,
-      successCallback: (data: any, textStatus: string, jqXHR: JQueryXHR) => any,
-      failureCallback: (ev: Event) => void
-   )
-   {
-      var url = this._baseUrl + "/studies/" + studyInstanceUid + "/metadata";
-      $.get(url, successCallback, "json");
-   }
-
    public getStudy
    (
       studyInstanceUid: string,
       mediaType: string,
-      successCallback: (data: any, textStatus: string) => any,
-      failureCallback: (ev: Event) => void
-   )
+      transferSyntax: string = null
+   ): JQueryPromise<{}>
    {
-      this.getDICOMMultipart("/studies/" + studyInstanceUid + "/",
-         mediaType,
-         successCallback,
-         failureCallback);
+      return this.getBinaryDICOMMultipart ( "/studies/" + studyInstanceUid + "/",
+                                      mediaType, transferSyntax );
    }
 
-   public getObjectInstanceMetadata
+   public getSeries
    (
       studyInstanceUid: string,
       seriesInstanceUid: string,
-      sopInstanceUID: string,
-      mediaType:string,
-      successCallback: (data: any, textStatus: string, jqXHR: JQueryXHR) => any,
-      failureCallback?: (ev: Event) => void
-   )
+      mediaType: string,
+      transferSyntax: string = null
+   ): JQueryPromise<{}>
    {
-      var url = this._baseUrl + "/studies/" + studyInstanceUid + "/series/" + seriesInstanceUid + "/instances/" + sopInstanceUID + "/metadata";
-      if (!mediaType || mediaType == MimeTypes.Json) {
-         $.get(url, successCallback, "json");
-      }
-      else if (mediaType==MimeTypes.xmlDicom)
-      {
-         this.getDICOMMultipart("/studies/" + studyInstanceUid + "/series/" + seriesInstanceUid + "/instances/" + sopInstanceUID + "/",
-            mediaType,
-            successCallback,
-            failureCallback);
-      }
+      return this.getBinaryDICOMMultipart ( "/studies/" + studyInstanceUid + "/" + "series" + "/",
+                                      mediaType, transferSyntax );
    }
-
 
    public getObjectInstance
    (
@@ -68,14 +42,77 @@ class WadoRsProxy{
       seriesInstanceUid: string,
       sopInstanceUID: string,
       mediaType: string,
-      successCallback: (data: any, textStatus?: string, jqXHR?: JQueryXHR) => any,
-      failureCallback: (ev: Event) => void
+      transferSyntax: string = null
+   ): JQueryPromise<{}>
+   {
+      return this.getBinaryDICOMMultipart("/studies/" + studyInstanceUid + "/series/" + seriesInstanceUid + "/instances/" + sopInstanceUID + "/",
+         mediaType, transferSyntax);
+   }
+
+
+   public getStudyMetadata
+   (
+      studyInstanceUid: string,
+      mediaType: string = null,
+      transferSyntax: string = null
    )
    {
-      this.getDICOMMultipart("/studies/" + studyInstanceUid + "/series/" + seriesInstanceUid + "/instances/" + sopInstanceUID + "/",
-         mediaType,
-         successCallback,
-         failureCallback);
+      var urlParts = "/studies/" + studyInstanceUid + "/metadata";
+
+      return this.getMetadata(urlParts, mediaType, transferSyntax);
+   }
+
+   public getSeriesMetadata
+   (
+      studyInstanceUid:string,
+      seriesInstanceUid: string,
+      mediaType: string = null,
+      transferSyntax: string = null
+   )
+   {
+      var urlParts = "/studies/" + studyInstanceUid + "/series/" + seriesInstanceUid + "/metadata";
+
+      return this.getMetadata(urlParts, mediaType, transferSyntax);
+   }
+
+
+   public getObjectInstanceMetadata
+   (
+      studyInstanceUid: string,
+      seriesInstanceUid: string,
+      sopInstanceUid: string,
+      mediaType: string,
+      transferSyntax: string = null
+   )
+   {
+      var urlParts = "/studies/" + studyInstanceUid + "/series/" + seriesInstanceUid + "/instances/" + sopInstanceUid + "/metadata";
+
+      return this.getMetadata(urlParts, mediaType, transferSyntax);
+   }
+
+
+   public getMetadata
+   (
+      urlParts: string,
+      mediaType: string = null,
+      transferSyntax: string = null
+   ): JQueryPromise<{}>
+   {
+      if (mediaType === null || typeof (mediaType) === "undefined") {
+         mediaType = MimeTypes.Json;
+      }
+
+
+      if (mediaType == MimeTypes.Json) {
+
+         return this.get(urlParts, mediaType, transferSyntax);
+      }
+      else if (mediaType == MimeTypes.xmlDicom) {
+         return this.getMultipart(urlParts, mediaType, transferSyntax);
+      }
+      else {
+         throw "Invalid mediaType for metadata request";
+      }
    }
 
    public getFrame
@@ -85,113 +122,92 @@ class WadoRsProxy{
       sopInstanceUID: string,
       frameList:string,
       mediaType: string,
-      successCallback: (data: any, textStatus?: string, jqXHR?: JQueryXHR) => any,
-      failureCallback: (ev: Event) => void
-   )
+      transferSyntax: string = null
+   ): JQueryPromise<{}>
    {
-      this.getDICOMMultipart("/studies/" + studyInstanceUid + "/series/" + seriesInstanceUid + "/instances/" + sopInstanceUID +
+      return this.getBinaryDICOMMultipart("/studies/" + studyInstanceUid + "/series/" + seriesInstanceUid + "/instances/" + sopInstanceUID +
          "/frames/" + frameList,
-         mediaType,
-         successCallback,
-         failureCallback);
+         mediaType);
    }
 
-   public getFrameUncompressed
-   (
-      studyInstanceUid: string,
-      seriesInstanceUid: string,
-      sopInstanceUID: string,
-      frameList: string,
-      successCallback: (data: any, textStatus?: string, jqXHR?: JQueryXHR) => any,
-      failureCallback: (ev: Event) => void
-   ) {
-      this.getFrame(studyInstanceUid, seriesInstanceUid, sopInstanceUID, frameList, "application/octet-stream",
-         successCallback,
-         failureCallback);
-   }
-
-   public getObjectUncompressed
-   (
-       studyInstanceUid: string,
-       seriesInstanceUid: string,
-       sopInstanceUID: string,
-       successCallback: (data: any, textStatus?: string, jqXHR?: JQueryXHR) => any,
-       failureCallback: (ev: Event) => void
-   )
-   {
-      this.getObjectInstance(studyInstanceUid, seriesInstanceUid, sopInstanceUID,
-                             "application/octet-stream", successCallback, failureCallback);
-   }
-
-   public getObjectDicom
-   (
-      studyInstanceUid: string,
-      seriesInstanceUid: string,
-      sopInstanceUID: string,
-      successCallback: (data: any, textStatus?: string, jqXHR?: JQueryXHR) => any,
-      failureCallback: (ev: Event) => void
-   ) {
-      this.getObjectInstance(studyInstanceUid, seriesInstanceUid, sopInstanceUID,
-         "application/dicom",
-         successCallback,
-         failureCallback);
-   }
-
-
-
-   public getStudyDicom
-   (
-      studyInstanceUid: string,
-      successCallback: (data: any, textStatus?: string, jqXHR?: JQueryXHR) => any,
-      failureCallback: (ev: Event) => void
-   ) {
-      this.getStudy( studyInstanceUid,
-         "application/dicom",
-         successCallback,
-         failureCallback);
-   }
-
-   public getStudyUncompressed
-   (
-       studyInstanceUid: string,
-       successCallback: (data: any, textStatus?: string, jqXHR?: JQueryXHR) => any,
-       failureCallback: (ev: Event) => void
-   )
-   {
-      this.getStudy(studyInstanceUid,
-         "application/octet-stream",
-         successCallback,
-         failureCallback);
-   }
-
-   public getDICOMMultipart
+   public getBinaryDICOMMultipart
    (
       urlRsPart: string,
       acceptDataType: string,
-      successCallback: (data: any, textStatus?: string, jqXHR?: JQueryXHR) => any,
-      failureCallback: (ev: Event) => void
-   )
+      transferSyntax:string = null
+   ): JQueryPromise<{}>
    {
+      var deffered = $.Deferred();
       var url = this._baseUrl + urlRsPart;
       var xhr = new XMLHttpRequest();
+      var acceptHeader = "multipart/related; type=\"" + acceptDataType + "\"";
+
+      if (transferSyntax)
+      {
+         acceptHeader += ";transfer-syntax=" + transferSyntax ;
+      }
 
       xhr.overrideMimeType("application/octet-stream");
       xhr.open("GET", url, true);
       xhr.responseType = "arraybuffer";
-      xhr.setRequestHeader("accept", "multipart/related; type=\"" + acceptDataType + "\"");
+      xhr.setRequestHeader("accept", acceptHeader);
       xhr.onreadystatechange = function () {
          if (xhr.readyState == 4 && xhr.status == 200) {
             // Fire event
             //listener.onDataReceived(new Uint8Array(xhr.response));
             var buffer = new Uint8Array(xhr.response);
 
-            successCallback(buffer, xhr.statusText );
+            deffered.resolve(buffer, xhr.statusText);
          }
       };
       xhr.onerror = function (error) {
-         failureCallback(error);
+         deffered.reject(error);
       };
       xhr.send(null);
 
+      return deffered.promise();
    }
+
+   public getMultipart
+   (
+      urlRsPart: string,
+      acceptDataType: string,
+      transferSyntax: string = null
+   ): JQueryPromise<{}>
+   {
+      return this.get(urlRsPart, "multipart/related; type=\"" + acceptDataType + "\"", transferSyntax);
+   }
+
+   public get
+   (
+      urlRsPart: string,
+      acceptHeader: string,
+      transferSyntax:string = null
+   ): JQueryPromise<{}>
+   {
+      var deffered = $.Deferred();
+      var url = this._baseUrl + urlRsPart;
+      var xhr = new XMLHttpRequest();
+
+
+      if (transferSyntax) {
+         acceptHeader += ";transfer-syntax=" + transferSyntax;
+      }
+      
+      xhr.open("GET", url, true);
+      xhr.setRequestHeader("accept", acceptHeader);
+      xhr.onreadystatechange = function () {
+         if (xhr.readyState == 4 && xhr.status == 200) {
+            deffered.resolve(xhr.response, xhr.statusText);
+         }
+      };
+      xhr.onerror = function (error) {
+         deffered.reject(error);
+      };
+      xhr.send(null);
+
+      return deffered.promise();
+   }
+
+ 
 }
