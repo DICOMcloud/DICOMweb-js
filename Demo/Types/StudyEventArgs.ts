97a54e6c5d8b7b13ad2865ccd9341d6594835e36
zaid al-safadi <zsafadi11@gmail.com>
2017-02-20 17:18:59 -0500

    Include missing file and Fix build error

---
+++ Demo/Types/StudyEventArgs.ts
@@ -0,0 +1,14 @@
+﻿class StudyEventArgs {
+   constructor(study: StudyParams) {
+      this._studyParams = study;
+   }
+
+   private _studyParams: StudyParams;
+
+   public get StudyParams(): StudyParams{
+      return this._studyParams;
+   };
+   public set StudyParams(instance: StudyParams) {
+      this._studyParams = instance;
+   };
+}
