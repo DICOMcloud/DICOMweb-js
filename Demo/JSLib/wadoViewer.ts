916cc7bed713336202b98d299389e8b562e3e833
zaid al-safadi <zsafadi@hotmail.com>
2017-09-11 20:38:11 -0400

    Fix Demo display errors on the console window "getEnabledElement: parameter element must not be undefined" issue #3

--- Demo/JSLib/wadoViewer.ts
+++ Demo/JSLib/wadoViewer.ts
@@ -30,8 +30,8 @@ class WadoViewer
       cornerstone.enable(this._viewerElement);
 
       this.configureWebWorker();
-
-      $(window).resize(function () {
+      
+      $(window).resize(() => {
          cornerstone.resize(this._viewerElement, true);
       })
    }
@@ -85,78 +85,79 @@ class WadoViewer
 
        try {
            var start = new Date().getTime();
-           cornerstone.loadAndCacheImage(imageId).then( (image:any)=> {
-               console.log(image);
-               var viewport = cornerstone.getDefaultViewportForImage(element, image);
-               //$('#toggleModalityLUT').attr("checked",viewport.modalityLUT !== undefined);
-               //$('#toggleVOILUT').attr("checked",viewport.voiLUT !== undefined);
-               cornerstone.displayImage(element, image, viewport);
-               if(this._loaded === false) {
-                   cornerstoneTools.mouseInput.enable(element);
-                   cornerstoneTools.mouseWheelInput.enable(element);
-                   cornerstoneTools.wwwc.activate(element, 1); // ww/wc is the default tool for left mouse button
-                   cornerstoneTools.pan.activate(element, 2); // pan is the default tool for middle mouse button
-                   cornerstoneTools.zoom.activate(element, 4); // zoom is the default tool for right mouse button
-                   cornerstoneTools.zoomWheel.activate(element); // zoom is the default tool for middle mouse wheel
-                   cornerstoneTools.wwwcTouchDrag.activate(element);
-                   this._loaded = true;
-               }
+           cornerstone.loadAndCacheImage(imageId).done((image: any) => {
+              console.log(image);
+              var viewport = cornerstone.getDefaultViewportForImage(element, image);
+              //$('#toggleModalityLUT').attr("checked",viewport.modalityLUT !== undefined);
+              //$('#toggleVOILUT').attr("checked",viewport.voiLUT !== undefined);
+              cornerstone.displayImage(element, image, viewport);
+              if (this._loaded === false) {
+                 cornerstoneTools.mouseInput.enable(element);
+                 cornerstoneTools.mouseWheelInput.enable(element);
+                 cornerstoneTools.wwwc.activate(element, 1); // ww/wc is the default tool for left mouse button
+                 cornerstoneTools.pan.activate(element, 2); // pan is the default tool for middle mouse button
+                 cornerstoneTools.zoom.activate(element, 4); // zoom is the default tool for right mouse button
+                 cornerstoneTools.zoomWheel.activate(element); // zoom is the default tool for middle mouse wheel
+                 cornerstoneTools.wwwcTouchDrag.activate(element);
+                 this._loaded = true;
+              }
 
-               function getTransferSyntax() {
-                   var value = image.data.string('x00020010');
-                   return value + ' [' + uids[value] + ']';
-               }
+              function getTransferSyntax() {
+                 var value = image.data.string('x00020010');
+                 return value + ' [' + uids[value] + ']';
+              }
 
-               function getSopClass() {
-                   var value = image.data.string('x00080016');
-                   return value + ' [' + uids[value] + ']';
-               }
+              function getSopClass() {
+                 var value = image.data.string('x00080016');
+                 return value + ' [' + uids[value] + ']';
+              }
 
-               function getPixelRepresentation() {
-                   var value = image.data.uint16('x00280103');
-                   if(value === undefined) {
-                       return;
-                   }
-                   return value + (value === 0 ? ' (unsigned)' : ' (signed)');
-               }
+              function getPixelRepresentation() {
+                 var value = image.data.uint16('x00280103');
+                 if (value === undefined) {
+                    return;
+                 }
+                 return value + (value === 0 ? ' (unsigned)' : ' (signed)');
+              }
 
-               function getPlanarConfiguration() {
-                   var value = image.data.uint16('x00280006');
-                   if(value === undefined) {
-                       return;
-                   }
-                   return value + (value === 0 ? ' (pixel)' : ' (plane)');
-               }
+              function getPlanarConfiguration() {
+                 var value = image.data.uint16('x00280006');
+                 if (value === undefined) {
+                    return;
+                 }
+                 return value + (value === 0 ? ' (pixel)' : ' (plane)');
+              }
 
 
-               $('#transferSyntax').text(getTransferSyntax());
-               $('#sopClass').text(getSopClass());
-               $('#samplesPerPixel').text(image.data.uint16('x00280002'));
-               $('#photometricInterpretation').text(image.data.string('x00280004'));
-               $('#numberOfFrames').text(image.data.string('x00280008'));
-               $('#planarConfiguration').text(getPlanarConfiguration());
-               $('#rows').text(image.data.uint16('x00280010'));
-               $('#columns').text(image.data.uint16('x00280011'));
-               $('#pixelSpacing').text(image.data.string('x00280030'));
-               $('#bitsAllocated').text(image.data.uint16('x00280100'));
-               $('#bitsStored').text(image.data.uint16('x00280101'));
-               $('#highBit').text(image.data.uint16('x00280102'));
-               $('#pixelRepresentation').text(getPixelRepresentation());
-               $('#windowCenter').text(image.data.string('x00281050'));
-               $('#windowWidth').text(image.data.string('x00281051'));
-               $('#rescaleIntercept').text(image.data.string('x00281052'));
-               $('#rescaleSlope').text(image.data.string('x00281053'));
-               $('#basicOffsetTable').text(image.data.elements.x7fe00010.basicOffsetTable ? image.data.elements.x7fe00010.basicOffsetTable.length : '');
-               $('#fragments').text(image.data.elements.x7fe00010.fragments ? image.data.elements.x7fe00010.fragments.length : '');
-               $('#minStoredPixelValue').text(image.minPixelValue);
-               $('#maxStoredPixelValue').text(image.maxPixelValue);
-               var end = new Date().getTime();
-               var time = end - start;
-               $('#loadTime').text(time + "ms");
-            
-           }, function(err) {
-              new ModalDialog().showError("Error", err);
-           });
+              $('#transferSyntax').text(getTransferSyntax());
+              $('#sopClass').text(getSopClass());
+              $('#samplesPerPixel').text(image.data.uint16('x00280002'));
+              $('#photometricInterpretation').text(image.data.string('x00280004'));
+              $('#numberOfFrames').text(image.data.string('x00280008'));
+              $('#planarConfiguration').text(getPlanarConfiguration());
+              $('#rows').text(image.data.uint16('x00280010'));
+              $('#columns').text(image.data.uint16('x00280011'));
+              $('#pixelSpacing').text(image.data.string('x00280030'));
+              $('#bitsAllocated').text(image.data.uint16('x00280100'));
+              $('#bitsStored').text(image.data.uint16('x00280101'));
+              $('#highBit').text(image.data.uint16('x00280102'));
+              $('#pixelRepresentation').text(getPixelRepresentation());
+              $('#windowCenter').text(image.data.string('x00281050'));
+              $('#windowWidth').text(image.data.string('x00281051'));
+              $('#rescaleIntercept').text(image.data.string('x00281052'));
+              $('#rescaleSlope').text(image.data.string('x00281053'));
+              $('#basicOffsetTable').text(image.data.elements.x7fe00010.basicOffsetTable ? image.data.elements.x7fe00010.basicOffsetTable.length : '');
+              $('#fragments').text(image.data.elements.x7fe00010.fragments ? image.data.elements.x7fe00010.fragments.length : '');
+              $('#minStoredPixelValue').text(image.minPixelValue);
+              $('#maxStoredPixelValue').text(image.maxPixelValue);
+              var end = new Date().getTime();
+              var time = end - start;
+              $('#loadTime').text(time + "ms");
+
+           })
+            .fail(() => {
+                 new ModalDialog().showError("Error","Image Failed to load");
+              });
        }
        catch(err) {
           new ModalDialog().showError("Error", err);
